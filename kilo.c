3489a4cffcd80de87af56eac7c4e6bb173bccd0f
AnujJha88 <anujjha8788@gmail.com>
2025-12-19 11:33:17 +0530

    append buffer

--- kilo.c
+++ kilo.c
@@ -18,7 +18,12 @@ struct editorConfig{
 };
 
 struct editorConfig E;
+
+
+
 ///TERMINAL
+
+
 void die(const char* s){
     write(STDOUT_FILENO,"\x1b[2J",4);
     write(STDOUT_FILENO,"\x1b[H",3);
@@ -57,26 +62,24 @@ char editorReadKey(){
     return c;
 }
 int getCursorPosition(int *rows, int *cols){
+    char buf[32];
+    unsigned int i=0;
     if(write(STDOUT_FILENO,"\x1b[6n",4)!=4)return -1;
 
-    char c;
-    while(read(STDIN_FILENO,&c,1)==1){
-        if(iscntrl(c)){
-            printf("%d\r\n",c);
-
-        }
-        else{
-            printf("%d('%c')\r\n",c,c);
-        }
+  while(i<sizeof(buf)-1){
+        if(read(STDIN_FILENO,&buf[i],1)!=1)break;
+        if(buf[i]=='R')break;
+        i++;
     }
-
-    editorReadKey();
-    return -1;
+    buf[i]='\0';
+    if(buf[0]!='\x1b'||buf[1]!='[')return -1;
+    if(sscanf(&buf[2],"%d,%d",rows,cols)!=2)return -1;
+         return 0;
 }
 
 int getWindowSize(int* rows,int* cols){
     struct winsize ws;
-    if(1||ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1||ws.ws_col==0){
+    if(ioctl(STDOUT_FILENO,TIOCGWINSZ,&ws)==-1||ws.ws_col==0){
         if(write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12)!=12)return -1;
                return getCursorPosition(rows,cols);
     }
@@ -87,26 +90,77 @@ int getWindowSize(int* rows,int* cols){
     }
 }
 
+
+/// APPEND BUFFER
+
+struct abuf{
+    char* b;
+    int len;
+};
+
+
+#define ABUF_INIT {NULL,0} // an empty append buffer
+
+void abAppend(struct abuf* ab, int len, const char* s){
+    char* new= realloc(ab->b, ab->len+len);//make sure we have enough space
+
+    if(new==NULL) return;
+    //now we use memcpy to copy the new string into the buffer
+
+    memcpy(&new[ab->len], s,len);
+    ab->b=new;
+    ab->len+=len;
+
+}
+
+void abFree(struct abuf *ab){
+    free(ab->b);
+}
+
+
 ///OUTPUT
 
 
 
-void editorDrawRows(){
+void editorDrawRows(struct abuf *ab){
     int y;
     for(y=0;y<E.screenRows;y++){
-        write(STDOUT_FILENO,"~\r\n",3);
+        //write(STDOUT_FILENO,"~",3);
+        //if(y<E.screenRows-1)write(STDOUT_FILENO,"\r\n",2);
+        abAppend(ab, "~",1);
+        if(y<E.screenRows-1){
+            abAppend(ab,"\r\n",2);
+        }
     }
 
+
 }
+
 void editorRefresh(){
-    write(STDOUT_FILENO,"\x1b[2J",4);//clear screen fully. keeps the cursor at bottom
-    write(STDOUT_FILENO,"\x1b[H",3);//move cursor to top left
-    editorDrawRows();
-    write(STDOUT_FILENO,"\x1b[H",3);//move cursor to top left
+    //write(STDOUT_FILENO,"\x1b[2J",4);//clear screen fully. keeps the cursor at bottom
+    //write(STDOUT_FILENO,"\x1b[H",3);//move cursor to top left
+   // editorDrawRows();
+    //write(STDOUT_FILENO,"\x1b[H",3);//move cursor to top left
+
+    struct abuf ab=ABUF_INIT;
+      abAppend(&ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[2J",4);
+    abAppend(&ab, "\x1b[H",3);
+
+    editorDrawRows(&ab);
+
+    abAppend(&ab, "\x1b[H",3);
+
+      abAppend(&ab, "\x1b[?25h", 6);
+
+    write(STDOUT_FILENO,ab.b,ab.len);
+    abFree(&ab);
+
 }
 
 
 /// INPUT
+
 void editorProcessKey(){
     char c=editorReadKey();
     switch(c){
