a6e40ec5b214f2e6a60eb11d4d417eb416290411
AnujJha88 <anujjha8788@gmail.com>
2025-12-22 22:39:39 +0530

    home,del,end key

--- kilo.c
+++ kilo.c
@@ -17,9 +17,17 @@ enum editorKey{
     ARROW_UP,
     ARROW_DOWN,
     PAGE_UP,
-    PAGE_DOWN
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY,
+    DEL_KEY
 };
 /// DATA
+
+typedef struct erow{
+    int size;
+    char* chars;
+} erow;
 
 struct editorConfig{
     int cx,cy;
@@ -81,14 +89,16 @@ int  editorReadKey(){
                 }
                 if(seq[2]=='~'){
                     switch(seq[1]){
+                        case '1': return HOME_KEY;
+                        case '3':return DEL_KEY;
+                        case '4':return END_KEY;
                         case '5':return PAGE_UP;
                         case '6':return PAGE_DOWN;
+                        case '7':return HOME_KEY;
+                        case '8':return END_KEY;
                     }
                 }
 
-            if(read(STDIN_FILENO,&seq[2],1)!=1){
-                return '\x1b';
-            }
 
                             }
 
@@ -98,8 +108,16 @@ int  editorReadKey(){
                 case 'B':return ARROW_DOWN;
                 case 'C':return ARROW_RIGHT;
                 case 'D':return ARROW_LEFT;
+                case 'H':return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
+        }
+        else if(seq[0]=='O'){
+            switch(seq[1]){
+                case 'H':return HOME_KEY;
+                case 'F': return END_KEY;
+            }
         }
         return '\x1b';
     }
@@ -263,6 +281,13 @@ void editorProcessKey(){
         case ARROW_UP:
         case ARROW_DOWN:
         editorMoveCursor(c);
+        break;
+
+        case HOME_KEY:
+        E.cx=0;
+        break;
+        case END_KEY:
+        E.cx=E.screenRows-1;
         break;
     }
 }
